a6b6516f6617e87d24f516aa43256cf03c46844c
Herminater <hermanskovbjerg@gmail.com>
2024-01-22 14:09:02 +0100

    Added initial printstatements

--- recommendations.py
+++ recommendations.py
@@ -1,2 +1,12 @@
-def hi():
-    print("Hello")
+
+
+def recommend_software():
+    print("Hi, welcome to Hermans playlist recommender: \n")
+
+    print("""You will be provided with some options, like wheter you want to have recommendations
+          given based on you mood, liked genres, atuhor etc.
+          Then after you will be presented with possible songs that match you criterias.
+          You just have to say yes or no""")
+
+    
+    
